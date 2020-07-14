d87d8d63e0889bd00dfea5e6f3091ab24340ba57
Sara DeLessio <sara@zevross.com>
2020-07-14 10:12:25 -0400

    Initial commit for static frontend

---
+++ design/scripts.js
@@ -0,0 +1,27 @@
+
+var sideNavButton = document.getElementById("toggle-search-drawer-button");
+sideNavButton.addEventListener("click", toggleSidenav);
+var sideNav = document.getElementById("mySidenav");
+var closeNavButton = document.getElementById("close-nav-button");
+closeNavButton.addEventListener("click", closeSideNav);
+
+function toggleSidenav() {
+  if (
+    sideNav.style.transform === "translate(500px)" ||
+    sideNav.style.transform === ""
+  ) {
+    sideNav.style.transform = "translate(0px)";
+    sideNav.style.boxShadow = "0 19px 38px rgba(0, 0, 0, 0.3)";
+    sideNavButton.style.transform = "translate(0px)";
+  } else {
+    sideNav.style.transform = "translate(500px)";
+    sideNav.style.boxShadow = "none";
+    sideNavButton.style.transform = "translate(500px)";
+  }
+}
+
+function closeSideNav() {
+  sideNav.style.transform = "translate(500px)";
+  sideNav.style.boxShadow = "none";
+  sideNavButton.style.transform = "translate(500px)";
+}
